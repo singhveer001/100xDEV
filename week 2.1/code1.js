8c1f926a61872006de5d59ddd9a8f326060500eb
singhveer001 <veerbhansingh70@gmail.com>
2024-01-09 16:51:11 +0530

    Callback

---
+++ week 2.1/code1.js
@@ -0,0 +1,27 @@
+// The problem in this code is that here we repeating 
+// same type of functions(code) again and again That is 
+
+function square (n){
+    return n * n;
+}
+
+function cube(n){
+    return n * n * n
+}
+
+function sumOfSquare(a,b){
+    let square1 = square(a);
+    let square2 = square(b);
+    return square1 + square2;
+}
+function sumOfcube(a,b){
+    let cube1 = square(a);
+    let cube2 = square(b);
+    return cube1 + cube2;
+}
+
+let ans = sumOfSquare(2,4)
+console.log(ans);
+
+let ans2 = sumOfcube(2,4);
+console.log(ans2);
