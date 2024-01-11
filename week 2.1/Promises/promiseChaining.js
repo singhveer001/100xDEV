302e50645c38a2df81c6f7d00056df5c3b2ca39f
singhveer001 <veerbhansingh70@gmail.com>
2024-01-12 02:12:14 +0530

    Promise Chaining

---
+++ week 2.1/Promises/promiseChaining.js
@@ -0,0 +1,11 @@
+// This code is not complete code here we does not declared Promiose
+// Only understanding the 
+// Promise Chaining
+
+p.then(function(){
+    return newFunctionCall()
+}).then(function(){
+    return otherFun()
+}).then(function(){
+    return newOtherFun()
+})
