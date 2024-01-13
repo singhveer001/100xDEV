0be03ef9ddcbcbb23e85abb7f3befab33644745a
singhveer001 <veerbhansingh70@gmail.com>
2024-01-13 15:52:14 +0530

    .

--- week 2.2/http-server-1/index.js
+++ week 2.2/http-server-1/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
-const app = express(); 
+const app = express();  // Call the express function and get it in the app 
 const port = 3030
 
-app.get('/', function(req, res) {
+// Define the routes
+app.get('/', function(req, res) {  
+  // Do whatever you want here 
   res.send('Hello World!')
 })
 
+// start the server
 app.listen(port, function() {
   console.log(`Example app listening on port ${port}`)
 })
+
