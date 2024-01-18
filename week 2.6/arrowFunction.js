07f9f42e7c14d9325c6a44ceb094d6dc6461a7a4
singhveer001 <veerbhansingh70@gmail.com>
2024-01-18 20:36:18 +0530

    Arrow function

---
+++ week 2.6/arrowFunction.js
@@ -0,0 +1,24 @@
+// Arrow function
+
+// Normal function
+function sum(a,b){
+    return a+b;
+}
+
+// Arrow function
+const sum = () =>{
+    return a+b;
+}
+
+// Normal
+app.get('/',function(req,res){
+
+})
+
+// Arrow function
+app.get('/',(req,res) =>{
+
+})
+
+const ans = sum(1,2);
+console.log(sum);
