3237b007343b99438763e093e8bfc56820d04825
singhveer001 <veerbhansingh70@gmail.com>
2024-01-19 21:56:00 +0530

    Completed assignment creating fileServer

--- Assignments_/week-2/02-nodejs/fileServer.js
+++ Assignments_/week-2/02-nodejs/fileServer.js
@@ -7,7 +7,8 @@
     Example: GET http://localhost:3000/files
   2. GET /file/:filename - Returns content of given file by name
      Description: Use the filename from the request path parameter to read the file from `./files/` directory
-     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
+     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. 
+     Should return `File not found` as text if file is not found
      Example: GET http://localhost:3000/file/example.txt
     - For any other route not defined in the server return 404
     Testing the server - run `npm run test-fileServer` command in terminal
@@ -17,5 +18,37 @@
     const path = require('path');
     const app = express();
     
+    // const filesDirectory = path.join(__dirname, 'files');
+
+    app.get('/files',(req,res)=>{
+      fs.readdir(path.join(__dirname,'./files/'),(err,files)=>{
+        if(err){
+          console.log(err);
+        return res.status(500).json({ error: 'Failed to retrieve files' })
+        }else{
+          res.status(200).json(files);
+        }
+      })
+    })
+
+    app.get('/file/:filename',(req,res)=>{
+        const filename = req.params.filename;
+        const filePath = path.join(__dirname,'./files/',filename);
+        
+        fs.readFile(filePath,'utf-8',(err,data)=>{
+            if(err){
+             return res.status(404).send('File not found');
+            }
+           return res.status(200).send(data);
+        })
+    })
+
+    app.use((req, res) => {
+      res.status(404).send("Route not found");
+    });
     
+    // app.listen(4000,()=>{
+    //   console.log("http://localhost:4000")
+    // })
+
     module.exports = app;
