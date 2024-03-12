25f76119df993cc41a27baefcbdf0382f4b841d7
singhveer001 <veerbhansingh70@gmail.com>
2024-03-13 00:17:29 +0530

    added key

--- week 6.1/todo/src/App.jsx
+++ week 6.1/todo/src/App.jsx
@@ -53,7 +53,7 @@ function App() {
     
         <button onClick={addTodo}>Add a todo</button>
         {todos.map(function(todo) { 
-         return <Todo title={todo.title} description={todo.description}/>
+         return <Todo key={todo.id} title={todo.title} description={todo.description}/>
           })}
     </div>
   )
