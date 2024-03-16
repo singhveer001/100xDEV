2a9db102ce70cac9eb51b9eb5f34461ea1879e63
singhveer001 <veerbhansingh70@gmail.com>
2024-03-16 10:49:33 +0530

    useEffect completed

--- week 6.2/hooks/src/App2.jsx
+++ week 6.2/hooks/src/App2.jsx
@@ -2,24 +2,31 @@ import { useState,useEffect } from 'react'
 import axios from "axios";
 
 function App() {
-
+  const [selectedId,setSelectedId] = useState(1);
   return (
+    
     <div>
-        <Todo id={1} /> 
+      <button onClick={() => setSelectedId(1) }>1</button>
+      <button onClick={() => setSelectedId(2) }>2</button>
+      <button onClick={() => setSelectedId(3) }>3</button>
+      <button onClick={() => setSelectedId(4) }>4</button>
+      <Todo id={selectedId} /> 
     </div>
 )
 
 }
 
 function Todo({id}){
+
     const [todo, setTodos] = useState([])
 
+    // implement effect here
     useEffect( ()=>{
         axios.get("https://sum-server.100xdevs.com/todo?id=" + id)
           .then(function(response){
-            setTodos(response.data.todo)
+            setTodos(response.data.todo)  // in axios if you want to final thing then use response.data
           })
-      },[]);
+      },[id]);
 
     return <div>
       <h1>
