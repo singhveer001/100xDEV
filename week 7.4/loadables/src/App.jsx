b92e7ef30254807c8565cca13cdecc60d5c58aa1
singhveer001 <veerbhansingh70@gmail.com>
2024-04-06 14:24:31 +0530

    understanding Loadables in react recoil

---
+++ week 7.4/loadables/src/App.jsx
@@ -0,0 +1,36 @@
+
+import './App.css'
+import { RecoilRoot, useRecoilStateLoadable,useRecoilValueLoadable } from 'recoil';
+import { todosAtomFamily } from './atoms';
+
+function App() {
+  return <RecoilRoot>
+    <Todo id={1}/>
+    <Todo id={2} />
+  </RecoilRoot>
+}
+
+function Todo({id}) {
+  //  const [todo, setTodo] = useRecoilStateLoadable(todosAtomFamily(id));
+   const todo  = useRecoilValueLoadable(todosAtomFamily(id));
+   if (todo.state === "loading") {
+      return <div>loading..</div>
+   }
+   else if(todo.state === "hasValue"){
+    return (
+      <>
+        {todo.contents.title}
+        {todo.contents.description}
+        <br />
+      </>
+    )
+   }
+   else if(todo.state === "hasError"){
+    return <div>
+      Error while getting data from backend
+    </div>
+   }
+ 
+}
+
+export default App
