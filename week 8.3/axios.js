7a19c7c110fd7ccb0f90edfb30982eaa1890f077
singhveer001 <veerbhansingh70@gmail.com>
2024-04-18 02:47:12 +0530

    updated

--- week 8.3/axios.js
+++ week 8.3/axios.js
@@ -2,7 +2,7 @@ const axios = require("axios")
 
 // async function main(){
 //     const response = await fetch("https://sum-server.100xdevs.com/todos")
-//     const json = await respose.json();
+//     const json = await response.json();
 //     console.log(json.todos.length);
 // }
 
