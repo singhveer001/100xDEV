c4bacc384e8e1709fc1ac0cca317cdac5fbffc76
singhveer001 <veerbhansingh70@gmail.com>
2024-07-02 15:00:49 +0530

    Normalize line endings

--- week 2.6/arrowFunction.js
+++ week 2.6/arrowFunction.js
@@ -1,24 +1,24 @@
 // Arrow function
 
 // Normal function
-// function sum(a,b){
-//     return a+b;
-// }
+function sum(a,b){
+    return a+b;
+}
 
 // Arrow function
 const sum = (a,b) =>{
     return a+b;
 }
 
-// // Normal
-// app.get('/',function(req,res){
+// Normal
+app.get('/',function(req,res){
 
-// })
+})
 
-// // Arrow function
-// app.get('/',(req,res) =>{
+// Arrow function
+app.get('/',(req,res) =>{
 
-// })
+})
 
 const ans = sum(1,2);
 console.log(ans);
