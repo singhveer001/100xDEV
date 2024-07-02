bede8f69be4852b8a9753381dd1044563cb854c6
singhveer001 <veerbhansingh70@gmail.com>
2024-07-02 16:36:48 +0530

    how to manage files and some .tscongig.json changes

--- week 9.2/dist/intro_Typescript.js
+++ week 9.2/dist/intro_Typescript.js
@@ -1,19 +1,15 @@
 "use strict";
-// How to gives type to any variable
 const x = 101;
 console.log(x);
-// How to give types to argument of function
 function greet(firstName) {
     console.log("Hello" + firstName);
 }
 greet("Veer");
-// How to assign return type to a function
 function sum(a, b) {
     return a + b;
 }
 let value = sum(2, 3);
 console.log(value);
-// Function that take another function as input
 function runAfter1s(fn) {
     setTimeout(fn, 1000);
 }
