1f2fc763d336495224b9659303df9ac7bc205191
veerbhan <veerbhansingh70@gmail.com>
2025-04-24 00:02:24 +0530

    Learned how PostgreSQL works and implemented CRUD operations using Node.js and PostgreSQL

---
+++ Week 10 Postgress/src/index.ts
@@ -0,0 +1,77 @@
+ import { Client } from "pg";
+ import express from "express";
+
+ const app = express()
+ app.use(express.json())
+
+ const client = new Client ({
+    host : 'localhost',
+    port : 5432,
+    database:'test',
+    user : 'postgres',
+    password : 'Veerbhan@1'
+ })
+
+ client.connect()
+
+ app.post('/signup', async (req, res) => {
+    const {username, password,email} = req.body;
+    console.log("uuu",username,password,email)
+    const {city,country,pincode} = req.body
+
+    try {
+        const insertQuery = `INSERT INTO users (username , email, password) VALUES ($1,$2,$3) RETURNING id`
+        const insertAdressesQuery = `INSERT INTO adresses (city,country,pincode,user_id) values($1,$2,$3,$4)`
+        
+        await client.query("BEGIN;")  // Here the transaction starts
+        const response = await client.query(insertQuery,[username,email,password])
+        const userId = response.rows[0].id
+        const responseAdresses = await client.query(insertAdressesQuery,[city,country,pincode,userId])
+        await client.query("COMMIT;") // Here the transaction ends
+
+        res.json({
+            msg : "You have signed up"
+        })
+    } catch (error) {
+        console.log("Error While Signup",error)
+        res.json({
+            msg : "Error while signing up"
+        })
+    }
+ })
+
+// Get data from two tables without using join 
+app.get('/metadata', async (req,res) => {
+    const id = req.query.id;
+
+    const query1 =  `select * from users where id=$1`
+    const response = await client.query(query1,[id])
+
+    const query2 = `select * from adresses where user_id = $1`
+    const AddressResponse = await client.query(query2,[id])
+
+    res.json({
+        user : response.rows[0],
+        address : AddressResponse.rows[0]
+    })
+})
+
+// Getting data of two table using join
+app.get('/metadatajoin', async (req,res) => {
+    const id = req.query.id;
+
+    const query1 =  `
+            select u.username, u.email, a.city, a.country, a.pincode from users u 
+            join adresses a on u.id = a.user_id 
+            where u.id=$1`
+    const AddressResponse = await client.query(query1,[id])
+
+    res.json({
+        data : AddressResponse.rows
+    })
+})
+
+
+app.listen(4000,()=>{
+    console.log("Server Started")
+})
---
+++ Week 10 Postgress/src/practise.ts
@@ -0,0 +1,91 @@
+ import { Client } from "pg";
+
+ const client = new Client ({
+    host : 'localhost',
+    port : 5432,
+    database:'test',
+    user : 'postgres',
+    password : 'Veerbhan@1'
+ })
+
+ client.connect()
+
+ async function createUserTable(){
+  try {
+    const result = await client.query(`
+        create table users(
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(100) UNIQUE NOT NULL,
+            email VARCHAR(100) UNIQUE NOT NULL,
+            password VARCHAR(100) NOT NULL,
+            create_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        );    
+    `)
+    console.log("Table Created Successfully")
+    console.log(result)
+  } catch (error) {
+    console.log("Error While Creating table",error)
+  } finally {
+    await client.end()
+  }
+ }
+
+//  createUserTable();
+
+ async function insertData(username:string,password:string,email:string){
+    const data = await client.query(`insert into users (username,email,password) values ($1,$2,$3) returning *`,[username ,email, password])
+    if(data.rows.length > 0){
+        console.log("Inserted:", data.rows[0]);
+        console.log("Inserted:", data.rows[1]);
+        return data.rows[0];
+    }else{
+        console.log("No data inseted")
+        return null;
+    }
+
+ }
+ insertData('Vaibhav','Vaibhav@1','Vaibhav@gmail.com').catch(console.error)
+
+ async function getAllData (){
+    const result = await client.query(`select * from users`);
+    if (result) {
+        console.log(result.rows)
+    } else {
+        console.log("No data found")
+    }
+ }
+ getAllData()
+
+async function updateData(username: string, email: string, password?: string) {
+    try {
+      let query = `UPDATE users SET username = $1`;
+      const values: any[] = [username];
+      let paramIndex = 2;
+  
+      if (password) {
+        query += `, password = $${paramIndex}`;
+        values.push(password);
+        paramIndex++;
+      }
+  
+      query += ` WHERE email = $${paramIndex} RETURNING *`;
+      values.push(email);
+  
+      const result = await client.query(query, values);
+  
+      if (result.rows.length > 0) {
+        console.log("Updated:", result.rows[0]);
+        return result.rows[0];
+      } else {
+        console.log("No user found with that email.");
+        return null;
+      }
+  
+    } catch (err: any) {
+      console.error("Update error:", err.message);
+      return null;
+    }
+  }
+  
+  
+ updateData('sakhi',"Rahul@1")
