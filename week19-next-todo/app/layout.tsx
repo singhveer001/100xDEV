ef55046e26a720344d905261a5c20f97065bf35a
veerbhan <veerbhansingh70@gmail.com>
2025-04-30 17:12:53 +0530

    Learned why route group () is used and how it is useful in layout.tsx

--- week19-next-todo/app/layout.tsx
+++ week19-next-todo/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>Global Headerrrrr</div>
         {children}
+        <div>Global Footerrrrr</div>
       </body>
     </html>
   );
